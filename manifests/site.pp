1b46151c4a56f82b8496d7b5288be4de2395f060
root <root@jherbage1.mylabserver.com>
2018-01-25 14:36:46 +0000

    test of exec unless in site.pP

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node default {
   hiera_include('classes')
-  exec { '/usr/bin/touch /tmp/jason1': 
-    unless => '/usr/bin/ls /tmp/jason1' 
+  exec { '/bin/touch /tmp/jason1': 
+    unless => '/bin/ls /tmp/jason1' 
   }
 }
